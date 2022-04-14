c930bfed458e853d39d3051b323e3a882c77b9a6
Alexandre Yokoyama G Dias <alexandre.dias@aluno.unifafibe.edu.br>
2022-04-13 22:08:29 -0300

    Prova Finalizada!!!!

--- app/src/main/java/com/example/navegacaoomar/ArmeniaMain4.java
+++ app/src/main/java/com/example/navegacaoomar/ArmeniaMain4.java
@@ -13,6 +13,6 @@ public class ArmeniaMain4 extends AppCompatActivity {
         setContentView(R.layout.activity_armenia_main4);
 
         ImageView imgView =findViewById(R.id.imagem);
-        ///imgView.setImageResource(R.drawable.co);
+        ///?/imgView.setImageResource(R.drawable.co);
     }
 }
